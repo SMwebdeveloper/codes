c80d69ada6f79ebb5b7f5983d4dd2fb2ece3b532
Samandar <samandarwebdeveloper@gmail.com>
2023-11-07 21:55:36 +0500

    fix loader

--- app.js
+++ app.js
@@ -1,90 +1,46 @@
 "use strict";
 
-// const seriesDB = {
-//   count: 0,
-//   series: {},
-//   actors: {},
-//   geners: [],
-//   private: false,
-//   start: function () {
-//     seriesDB.count = +prompt("Nechta serial ko'rdingiz");
-//     if (
-//       seriesDB.count == null ||
-//       seriesDB.count == "" ||
-//       isNaN(seriesDB.count)
-//     ) {
-//       seriesDB.count = +prompt("Nechta serial ko'rdingiz");
-//     }
-//   },
-//   rememberMySerials: function () {
-//     const arr = ["Oxirgi ko'rgan serilanigiz", "Nechi baho berasiz"];
-//     for (let j = 0; j < 2; j++) {
-//       for (let i = 0; i < arr.length; i++) {
-//         const questions = prompt(`${arr[i]}`);
-//         if (questions != null && questions != "") {
-//           if (arr[i] == arr[i]) {
-//             seriesDB.series[arr[i]] = questions;
-//           }
-//         } else {
-//           console.log("error");
-//           i--;
-//         }
-//       }
-//     }
-//   },
-//   detectLevelSerial: function () {
-//     if (seriesDB.count <= 5) {
-//       console.log("Siz kam serial ko'ripsiz");
-//     } else if (10 > seriesDB.count > 5) {
-//       console.log("Siz classic tomoshabinsiz");
-//     } else if (seriesDB.count >= 10) {
-//       console.log("Siz professional serial ko'ruvchisiz");
-//     } else {
-//       console.log("error");
-//     }
-//   },
-//   showDB: function () {
-//     if (!seriesDB.private) {
-//       console.log(seriesDB);
-//     }
-//   },
-//   visibleDB: function () {
-//     if (seriesDB.private) {
-//       seriesDB.private = false;
-//     } else {
-//       seriesDB.private = true;
-//     }
-//   },
-//   writeGenres: function () {
-//     for (let i = 0; i < 3; i++) {
-//       const responseGenres = prompt("Yaxshi ko'rgan janringiz", "");
-
-//       if (responseGenres === "" || responseGenres === null) {
-//         console.log("Siz noto'g'ri ma'lumot kiritingiz");
-//         i--;
-//       } else {
-//           seriesDB.geners.push(responseGenres)
-//       }
-//     }
-//   },
-// };
+window.addEventListener("DOMContentLoaded", () => {
+  const tabsParent = document.querySelector(".tabheader__items"),
+    tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    loader = document.querySelector('.loader')
 
 
-// console.log(document.body.childNodes)
+    // Loader
+    setTimeout(() => {
+      loader.style.opacity = '0'
+        setTimeout(() => {
+          loader.style.display = 'none'
+      }, 500)
+    }, 2000)
 
-const arr = [
-    1,2,3,4
-]
+  function hideTabsContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide")
+      item.classList.remove("show")
+    });
 
-function arrFil(att) {
-    let result = att.slice(0,1) 
-    // att.forEach((item, index) => {
-    //     if (index == 0) {
-    //         result = item
-    //     }
-    // })
-
-    return result
-}
-
-console.log(arrFil(arr))
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+  function showTabContent(i = 0) {
+      tabsContent[i].classList.add("show")
+      tabsContent[i].classList.remove("hide")
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabsContent();
+  showTabContent();
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, idx) => {
+        if (target === item) {
+          hideTabsContent();
+          showTabContent(idx);
+        }
+      });
+    }
+  });
+});
